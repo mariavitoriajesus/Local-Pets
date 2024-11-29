e10b02a829a6f6d44db1364b0608b80b8b238f7a
Maria Vitória J. <102930406+mariavitoriajesus@users.noreply.github.com>
2024-11-29 17:41:16 -0300

    projetc complet

---
+++ pages/planos/script.js
@@ -0,0 +1,11 @@
+function login() {
+  //window.location.href = "construcao.html";
+  alert("Site em constução!! Por favor aguarde!")
+}
+
+function cadastrar() {
+    //window.location.href = "construcao.html";
+    alert("Site em construção!!")
+  }
+
+  setTimeout(redirecionar, 3000);
